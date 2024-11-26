ad7c053b9078af570759b2d9b0bdf49732c8f98e
Gustavo de oliveira <71619573+Gustavoo151@users.noreply.github.com>
2024-11-26 02:07:29 -0300

    Update SubgroupController.java
    Adicionando o outro site para o painel do tcc

--- src/main/java/com/dash/bpm/controller/SubgroupController.java
+++ src/main/java/com/dash/bpm/controller/SubgroupController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 
-@CrossOrigin(origins = "https://dashbpn.vercel.app/")
+@CrossOrigin(origins = "https://dashbpn.vercel.app/", "https://dashtcc.vercel.app/")
 
 @RestController
 @RequestMapping("/api/subgroups")
